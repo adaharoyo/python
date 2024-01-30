6618798931702b641aedd6959ac6d6a70c12636d
adaharoyo <nsongawaldah360@gmail.com>
2024-01-30 01:59:06 -0800

    Added phone file

---
+++ phones.py
@@ -0,0 +1,8 @@
+print("\t-----------------------------------------")
+print("\t|\t PHONES   \t\t\t|")
+print("\t|\t\t\t\t\t|")
+print("\t| Name:\tREDMI NOTE9\t\t\t|")
+print("\t| Colour:\tblue\t\t\t|")
+print("\t| Manufacturer:\tXiamoi\t\t\t|")
+print("\t| Brand:\tRedmi/POCO \t\t|")
+print("\t-----------------------------------------")
